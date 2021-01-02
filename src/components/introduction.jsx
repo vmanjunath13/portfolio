32c4c4a6e4dc78d9d776aacb23f0ecead6dbf1a2
vmanjunath13 <vmanjunath@hawk.iit.edu>
2021-01-02 07:39:16 +0530

    My Portfolio

---
+++ src/components/introduction.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{backgroundImage: 'url(images/bg4.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Hi! <br />I'm Vaishnavi</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1ZB4nmxg5aaMzQVA6RF-HwtIeOHBOtLQO/view?usp=sharing" target="_blank" rel="noopener noreferrer">View Resume<i className="icon-download4" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/bg4.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I love building<br /> THINGS !!</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/vmanjunath13/myprojects" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              {/* <li style={{backgroundImage: 'url(images/bg4.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I often do<br/> photography ... </h1>
+                          <nav>
+                              <p><a className="btn btn-primary btn-learn" href="#blog" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>                   
+                          </nav>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li> */}
+            </ul>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
